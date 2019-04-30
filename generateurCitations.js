f3ca7177f74bc9a38f02c348cc3d4bc0907bb168
alexis.peyron@gmail.com <alexispeyron@alexis-2.home>
2019-04-30 10:23:59 +0200

    indentation et commentaires

--- generateurCitations.js
+++ generateurCitations.js
@@ -1,34 +1,33 @@
-    const nbCitationChoisie = document.getElementById('nb_citations');
-    const CitationChoisie = document.getElementById('generateur_citations');
+const nbCitationChoisie = document.getElementById('nb_citations');
+
+const CitationChoisie = document.getElementById('generateur_citations');
     
-    const citationAffichee  = document.querySelector('.citation');
+const citationAffichee  = document.querySelector('.citation');
 
-    const para1 = document.createElement('p');
-    citationAffichee.appendChild(para1);
+const para1 = document.createElement('p');
+citationAffichee.appendChild(para1);
 
-    const para2 = document.createElement('p');
-    citationAffichee.appendChild(para2);
+const para2 = document.createElement('p');
+citationAffichee.appendChild(para2);
 
-    const para3 = document.createElement('p');
-    citationAffichee.appendChild(para3);
+const para3 = document.createElement('p');
+citationAffichee.appendChild(para3);
 
-    const para4 = document.createElement('p');
-    citationAffichee.appendChild(para4);
+const para4 = document.createElement('p');
+citationAffichee.appendChild(para4);
 
-    const para5 = document.createElement('p');
-    citationAffichee.appendChild(para5);
+const para5 = document.createElement('p');
+citationAffichee.appendChild(para5);
 
-    const para6 = document.createElement('p');
-    citationAffichee.appendChild(para6);
+const para6 = document.createElement('p');
+citationAffichee.appendChild(para6);
 
+const BoutonGenerer  = document.querySelector('button');
 
+BoutonGenerer.addEventListener('click', result);
 
-    const BoutonGenerer  = document.querySelector('button');
-
-    BoutonGenerer.addEventListener('click', result);
-
-
-    function result(){
+//génerer citaitons star wars ou Oss117
+function result(){
   switch(CitationChoisie.value){
     case "generateur_starwars":
     generateurStarwars();
@@ -39,13 +38,15 @@
   };
 };
 
+// choix du générateur
 $('#generateur_citations').change(function(){
   switch($('#generateur_citations option:selected').val()){
+    
     case "generateur_starwars":
     location.reload();
-  
-    case "generateur_Oss117":
-
+    
+    // si choix du générateur Oss117, charger l'image et la vidéo
+    case "generateur_Oss117":  
     $('body').css('background-image','url("../fetch_generateur_citations_configurable/images/oss117.jpg")')
     var p =  $('button').css('background-color','darkslateblue')
     $('iframe').attr('src','https://www.youtube.com/embed/59WbseO9AxM?autoplay=1')
--- generateurOss117.js
+++ generateurOss117.js
@@ -1,5 +1,6 @@
 function generateurOss117() {
   
+  //appel par la méthode fetch les ciations
     fetch('citationOss117.json')
     .then(function(response) {
       if (!response.ok) {
@@ -11,82 +12,82 @@ function generateurOss117() {
   
       class citationGenereeOss117 {
         constructor (insertX, insertY, insertZ){
-       this.insertX = Math.floor(Math.random()*json.citations.length);
-       this.insertY = Math.floor(Math.random()*json.citations1.length);
-      this.insertZ = Math.floor(Math.random()*json.citations2.length);
+        this.insertX = Math.floor(Math.random()*json.citations.length);
+        this.insertY = Math.floor(Math.random()*json.citations1.length);
+        this.insertZ = Math.floor(Math.random()*json.citations2.length);
         };
-     citationGenereeOss1171(){
-      para1.innerHTML = json.citations[this.insertX]
+        citationGenereeOss1171(){
+        para1.innerHTML = json.citations[this.insertX]
         .phrase + " - " + json.citations1[this.insertY].phrase + " - " + json.citations2[this.insertZ].phrase;
-     };
-     citationGenereeOss1172(){
-      para2.innerHTML = json.citations[this.insertX]
+        };
+        citationGenereeOss1172(){
+         para2.innerHTML = json.citations[this.insertX]
         .phrase + " - " + json.citations1[this.insertY].phrase + " - " + json.citations2[this.insertZ].phrase;
-     };
-     citationGenereeOss1173(){
-      para3.innerHTML = json.citations[this.insertX]
-        .phrase + " - " + json.citations1[this.insertY].phrase + " - " + json.citations2[this.insertZ].phrase;
-     };
-     citationGenereeOss1174(){
-      para4.innerHTML = json.citations[this.insertX]
-        .phrase + " - " + json.citations1[this.insertY].phrase + " - " + json.citations2[this.insertZ].phrase;
-     };
-     citationGenereeOss1175(){
-      para5.innerHTML = json.citations[this.insertX]
-        .phrase + " - " + json.citations1[this.insertY].phrase + " - " + json.citations2[this.insertZ].phrase;
-     };
+        };
+        citationGenereeOss1173(){
+        para3.innerHTML = json.citations[this.insertX]
+          .phrase + " - " + json.citations1[this.insertY].phrase + " - " + json.citations2[this.insertZ].phrase;
+        };
+        citationGenereeOss1174(){
+        para4.innerHTML = json.citations[this.insertX]
+          .phrase + " - " + json.citations1[this.insertY].phrase + " - " + json.citations2[this.insertZ].phrase;
+        };
+        citationGenereeOss1175(){
+        para5.innerHTML = json.citations[this.insertX]
+          .phrase + " - " + json.citations1[this.insertY].phrase + " - " + json.citations2[this.insertZ].phrase;
+        };
+      };
+
+  //création des 5 objets citations
+    function citationAfficheeOss1171() {
+      let citationOss1171 = new citationGenereeOss117;
+      citationOss1171.citationGenereeOss1171();
     };
 
-  function citationAfficheeOss1171() {
-    let citationOss1171 = new citationGenereeOss117;
-    citationOss1171.citationGenereeOss1171();
-  };
+    function citationAfficheeOss1172() {
+      let citationOss1172 = new citationGenereeOss117;
+     citationOss1172.citationGenereeOss1172();
+    };
 
-  function citationAfficheeOss1172() {
-    let citationOss1172 = new citationGenereeOss117;
-    citationOss1172.citationGenereeOss1172();
-  };
+    function citationAfficheeOss1173() {
+      let citationOss1173 = new citationGenereeOss117;
+      citationOss1173.citationGenereeOss1173();
+    };
 
-  function citationAfficheeOss1173() {
-    let citationOss1173 = new citationGenereeOss117;
-    citationOss1173.citationGenereeOss1173();
-  };
+    function citationAfficheeOss1174() {
+      let citationOss1174 = new citationGenereeOss117;
+      citationOss1174.citationGenereeOss1174();
+    };
 
-  function citationAfficheeOss1174() {
-    let citationOss1174 = new citationGenereeOss117;
-    citationOss1174.citationGenereeOss1174();
-  };
+    function citationAfficheeOss1175() {
+      let citationOss1175 = new citationGenereeOss117;
+      citationOss1175.citationGenereeOss1175();
+    };
 
-  function citationAfficheeOss1175() {
-    let citationOss1175 = new citationGenereeOss117;
-    citationOss1175.citationGenereeOss1175();
-  };
-
-  /*const texte = document.createTextNode('Citations Star Wars!')*/
-
+  
+    //affichages des citations en fonction de la valeur selectionnée
     switch (nbCitationChoisie.value) {
-    case "une":
-    citationAfficheeOss1171();
-      para1.style.display = "block";
-      para2.style.display = "none";
-      para3.style.display = "none";
-      para4.style.display = "none";
-      para5.style.display = "none";
-      /*para6.appendChild(texte);
-      break;*/
-    case "deux":
-    citationAfficheeOss1171();
-    citationAfficheeOss1172();
-      para1.style.display = "block";
-      para2.style.display = "block";
-      para3.style.display = "none";
-      para4.style.display = "none";
-      para5.style.display = "none";
-      break;
+      case "une":
+        citationAfficheeOss1171();
+        para1.style.display = "block";
+        para2.style.display = "none";
+        para3.style.display = "none";
+        para4.style.display = "none";
+        para5.style.display = "none";
+        break;
+      case "deux":
+        citationAfficheeOss1171();
+        citationAfficheeOss1172();
+        para1.style.display = "block";
+        para2.style.display = "block";
+        para3.style.display = "none";
+        para4.style.display = "none";
+        para5.style.display = "none";
+        break;
     case "trois":
-    citationAfficheeOss1171();
-    citationAfficheeOss1172();
-    citationAfficheeOss1173();
+      citationAfficheeOss1171();
+      citationAfficheeOss1172();
+      citationAfficheeOss1173();
       para1.style.display = "block";
       para2.style.display = "block";
       para3.style.display = "block";
@@ -94,10 +95,10 @@ function generateurOss117() {
       para5.style.display = "none";
       break;
     case "quatre":
-    citationAfficheeOss1171();
-    citationAfficheeOss1172();
-    citationAfficheeOss1173();
-    citationAfficheeOss1174();
+      citationAfficheeOss1171();
+      citationAfficheeOss1172();
+      citationAfficheeOss1173();
+      citationAfficheeOss1174();
       para1.style.display = "block";
       para2.style.display = "block";
       para3.style.display = "block";
@@ -105,11 +106,11 @@ function generateurOss117() {
       para5.style.display = "none";
       break;
     case "cinq":
-    citationAfficheeOss1171();
-    citationAfficheeOss1172();
-    citationAfficheeOss1173();
-    citationAfficheeOss1174();
-    citationAfficheeOss1175();
+      citationAfficheeOss1171();
+      citationAfficheeOss1172();
+      citationAfficheeOss1173();
+      citationAfficheeOss1174();
+      citationAfficheeOss1175();
       para1.style.display = "block";
       para2.style.display = "block";
       para3.style.display = "block";
